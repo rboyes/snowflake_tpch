c737d3cb84796ba8552a662cc1d52f11a85480ea
Rich Boyes <rich.boyes@admiralpioneer.com>
2025-12-30 11:38:05 +0000

    feat: change group name so not in default group

--- snowflake_tpch/snowflake_tpch/assets.py
+++ snowflake_tpch/snowflake_tpch/assets.py
@@ -1,11 +1,20 @@
+from typing import Any, Mapping
 from dagster import AssetExecutionContext
-from dagster_dbt import DbtCliResource, dbt_assets
+from dagster_dbt import DagsterDbtTranslator, DbtCliResource, DbtProject, dbt_assets
 
 from .project import snowflake_tpch_project
 
+dbt_project = DbtProject(snowflake_tpch_project.project_dir)
 
-@dbt_assets(manifest=snowflake_tpch_project.manifest_path)
+
+class CustomDagsterDbtTranslator(DagsterDbtTranslator):
+    def get_group_name(self, dbt_resource_props: Mapping[str, Any]) -> str | None:
+        return "snowflake_tpch"
+
+
+@dbt_assets(
+    manifest=snowflake_tpch_project.manifest_path,
+    dagster_dbt_translator=CustomDagsterDbtTranslator(),
+)
 def snowflake_tpch_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
-    
-
