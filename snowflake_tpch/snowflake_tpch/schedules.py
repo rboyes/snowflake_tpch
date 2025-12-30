f75679dbd1b06fab92598273a32eac21cacd9a37
Rich Boyes <rich.boyes@admiralpioneer.com>
2025-12-30 12:22:26 +0000

    turn on schedule

--- snowflake_tpch/snowflake_tpch/schedules.py
+++ snowflake_tpch/snowflake_tpch/schedules.py
@@ -6,11 +6,11 @@ from dagster_dbt import build_schedule_from_dbt_selection
 from .assets import snowflake_tpch_dbt_assets
 
 schedules = [
-#     build_schedule_from_dbt_selection(
-#         [snowflake_tpch_dbt_assets],
-#         job_name="materialize_dbt_models",
-#         cron_schedule="0 0 * * *",
-#         dbt_select="fqn:*",
-#     ),
+     build_schedule_from_dbt_selection(
+         [snowflake_tpch_dbt_assets],
+         job_name="snowflake_tpch_dbt_assets",
+         cron_schedule="0 13 * * *",
+         dbt_select="+fct_orders",
+     ),
 ]
 
