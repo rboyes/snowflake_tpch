61ab74d9238571437a422ab8f9db2e6e6b8adcc8
Rich Boyes <rich.boyes@admiralpioneer.com>
2025-12-31 09:29:23 +0000

    feat: refactoring into separate dbt folder

--- snowflake_tpch/snowflake_tpch/project.py
+++ snowflake_tpch/snowflake_tpch/project.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from dagster_dbt import DbtProject
 
 snowflake_tpch_project = DbtProject(
-    project_dir=Path(__file__).joinpath("..", "..", "..").resolve(),
+    project_dir=Path(__file__).joinpath("..", "..", "..", "dbt").resolve(),
     packaged_project_dir=Path(__file__).joinpath("..", "..", "dbt-project").resolve(),
 )
 snowflake_tpch_project.prepare_if_dev()
