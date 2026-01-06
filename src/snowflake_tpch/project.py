8989add9b7373d4f4e0ad23fa1fded7c723126c7
Rich Boyes <rich.boyes@admiralpioneer.com>
2026-01-06 09:24:37 +0000

    fix: need to use packaged project on deployment

--- src/snowflake_tpch/project.py
+++ src/snowflake_tpch/project.py
@@ -5,8 +5,10 @@ from dagster_dbt import DbtProject
 repo_project_dir = Path(__file__).joinpath("..", "..", "..", "dbt").resolve()
 packaged_project_dir = Path(__file__).joinpath("..", "dbt-project").resolve()
 
+project_dir = repo_project_dir if repo_project_dir.exists() else packaged_project_dir
+
 snowflake_tpch_project = DbtProject(
-    project_dir=repo_project_dir,
+    project_dir=project_dir,
     packaged_project_dir=packaged_project_dir,
 )
 snowflake_tpch_project.prepare_if_dev()
